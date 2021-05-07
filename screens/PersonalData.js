ae2b81b9cb1bb12c71da731823b6c8c9269bfda8
Emanuel Caprariu <emanuel.caprariu@student.upt.ro>
2021-05-07 20:36:38 +0300

    adding medical data design only

--- screens/PersonalData.js
+++ screens/PersonalData.js
@@ -1,19 +1,32 @@
 import React from 'react'
-import { Text, View, StyleSheet ,TouchableOpacity , ImageBackground, Keyboard, TouchableWithoutFeedback , ScrollView} from 'react-native'
+import { Text, View, StyleSheet ,TouchableOpacity , ImageBackground, Keyboard, TouchableWithoutFeedback , ScrollView,  Image} from 'react-native'
 import {Header, Left, Right, Icon} from 'native-base'
-import { MaterialIcons} from '@expo/vector-icons';
 import {globallyStyles} from '../global/styles';
-
+import { FontAwesome5 } from '@expo/vector-icons';
+import {MaterialIcons ,AntDesign ,FontAwesome ,MaterialCommunityIcons ,Ionicons} from '@expo/vector-icons';
 import Card from '../shared/card';
 
 export default class PersonalData extends React.Component{
+
+    date_medicalePress= () =>{
+        console.log("Merg datele medicale");
+    }
+    date_ambientalePress= () =>{
+        console.log("Merg datele ambientale");
+    }
+    diagnosticPress= () =>{
+        console.log("Merge diagnosticul");
+    }
+    date_istoricePress= () =>{
+        console.log("Merg datele istorice");
+    }
     render() {
         return (                   
           <TouchableWithoutFeedback onPress={()=>{
               Keyboard.dismiss();
           }}>
             <ImageBackground source={require("../assets/GREEN.png")} style={{flex:1}}>
-                <ScrollView style={styles.container}>
+                <View style={styles.container}>
                   
                     {/* aici vom salva datele introduse de catre pacient */}
                     <TouchableOpacity onPress = {() => this.props.navigation.openDrawer()} style={globallyStyles.menu} >
@@ -22,32 +35,50 @@ export default class PersonalData extends React.Component{
                     <View style = {styles.textMessage}>
                         <Text style={{fontSize:18,textAlign:'left',color:'#333',letterSpacing:2}}>Rezultate medicale</Text>
                     </View> 
-                    <View style={styles.card}>
-                        <TouchableOpacity>
+                    <ScrollView style={styles.card}>                       
                             <Card>
-                                <Text>text</Text>
-                            </Card>                          
-                        </TouchableOpacity>
-                        <TouchableOpacity>
+                                <Text style={styles.text}>Date medicale</Text>
+                                <TouchableOpacity onPress={() =>this.date_medicalePress()}>
+                                    <Image source={require("../assets/medical_dates.png")}  style={{ width: 120,height:120,marginLeft:20}} />
+                                </TouchableOpacity>                        
+                                <View style={{flex:0,flexDirection:'row',marginLeft:-40, justifyContent:'space-between'}}>
+                                    <MaterialCommunityIcons name="temperature-celsius" size={24} color="black"  />
+                                    <FontAwesome name="heartbeat" size={24} color="#d63031" />
+                                    <MaterialCommunityIcons name="weight-kilogram" size={24} color="black" />
+                                    <Ionicons name="water" size={24} color="#2980b9"/>
+                                    
+                                </View>                             
+                            </Card>                                                               
                             <Card>
-                                <Text>text</Text>
-                            </Card>   
-                        </TouchableOpacity>  
-                        <TouchableOpacity>
+                                <Text style={styles.text}>Date ambientale</Text>
+                                <TouchableOpacity onPress={() =>this.date_ambientalePress()}>
+                                    <Image source={require("../assets/growth.png")}  style={{ width: 120,height:120,marginLeft:20}} />
+                                </TouchableOpacity>                        
+                                <View style={{flex:0,flexDirection:'row',marginLeft:-40, justifyContent:'space-between'}}>
+                                    <MaterialCommunityIcons name="temperature-celsius" size={24} color="black"  />
+                                    <Ionicons name="water" size={24} color="#2980b9"/>
+                                    <FontAwesome name="fire" size={24} color="#d35400" />
+                                    <MaterialIcons name="house" size={24} color="#c0392b" />   
+                                    
+                                </View> 
+                            </Card>                                                  
                             <Card>
-                                <Text>text</Text>
-                            </Card>   
-                        </TouchableOpacity> 
-                        <TouchableOpacity>
+                                <Text style={styles.text}>Vezi diagnostic</Text>
+                                <TouchableOpacity  onPress={() =>this.diagnosticPress()}>
+                                    <Image source={require("../assets/medical_.png")}  style={{ width: 130,height:120,marginLeft:20}} />
+                                </TouchableOpacity>                        
+                               
+                            </Card>                                                 
                             <Card>
-                                <Text>text</Text>
-                            </Card>   
-                        </TouchableOpacity>                                    
-                    </View>
-                </ScrollView>
+                                <Text style={styles.text}>Vezi istoric date</Text>
+                                <TouchableOpacity  onPress={() =>this.date_istoricePress()}>
+                                    <Image source={require("../assets/clock.png")}  style={{ width: 120,height:120,marginLeft:20}} />
+                                </TouchableOpacity> 
+                            </Card>                                                             
+                    </ScrollView>
+                </View>
               </ImageBackground>
-          </TouchableWithoutFeedback>
-            
+          </TouchableWithoutFeedback>           
         )
     }
 }
@@ -58,13 +89,21 @@ const styles = StyleSheet.create({
         
     },
     card:{
-        flex:1,
-        flexDirection:'row',
+        flex:0,  
+        padding:10,
+        marginVertical:10,
     },  
     textMessage: {
-        flex: 1,
+        flex:1,
         alignItems: 'center',
         justifyContent:'center',
+       
+    },
+    text:{
+        fontSize:20,
+        marginBottom:10,
+        color:'#333',
+        letterSpacing:3,
     }
 
 })
--- shared/card.js
+++ shared/card.js
@@ -15,13 +15,20 @@ const styles = StyleSheet.create({
     card:{
         borderRadius: 6,
         elevation:3,
-        backgroundColor:'#4834d4',
+        backgroundColor:'#2ecc71',
         shadowOffset: { width:1, heigth:1},
         shadowColor:'#333',
         shadowOpacity:0.3,
         marginHorizontal:4,
         shadowRadius:4,
         marginVertical:10,
+        paddingVertical:20,
+        alignItems:'center',       
+        borderWidth:1,
+        borderColor:'#1abc9c',
+        width:300,
+        marginLeft:20,       
+        
     },
     cardContent:{
         marginHorizontal:18,
